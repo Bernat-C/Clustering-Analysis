37d421e49ae3195c3ac3ea6e704d6c9b2bb35d5e
Bernat <girobernat@gmail.com>
2024-11-23 19:54:49 +0100

    Added vowel preprocessing

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -8,38 +8,39 @@ def preprocess_sick():
     Applies the specified preprocessings for the dataset sick and stores it in the file datasets_processed/sick.csv.
     :return: dataframe
     """
-    for fold in range(10):
-        # Load arff files
-        df_sick, meta_train = loadarff(f'datasets/sick.arff')
+    # Load arff file
+    df_sick, meta_train = loadarff(f'datasets/sick.arff')
 
-        # Define datasets
-        df_sick = pd.DataFrame(df_sick)
+    # Define datasets
+    df_sick = pd.DataFrame(df_sick)
 
-        # Decode utf8 columns
-        for col in df_sick.columns:
-            df_sick[col].map(lambda x: x.decode('utf-8') if isinstance(x, bytes) else x)
+    # Decode utf8 columns
+    for col in df_sick.columns:
+        df_sick[col].map(lambda x: x.decode('utf-8') if isinstance(x, bytes) else x)
 
-        # Pick up the columns that have nans
-        columns_with_nans = []
-        for col in df_sick.columns:
-            sum_nans = sum(df_sick[col].isna())
-            percentage = sum_nans / len(df_sick) * 100
-            if (percentage > 0.1) and (percentage < 99):
-                columns_with_nans.append(col)
+    # Pick up the columns that have nans
+    columns_with_nans = []
+    for col in df_sick.columns:
+        sum_nans = sum(df_sick[col].isna())
+        percentage = sum_nans / len(df_sick) * 100
+        if (percentage > 0.1) and (percentage < 99):
+            columns_with_nans.append(col)
 
-        # We remove the rows where the age was null (there is only one)
-        df_sick = drop_rows(df_sick, ['age'])
+    # We remove the rows where the age was null (there is only one)
+    df_sick = drop_rows(df_sick, ['age'])
 
-        df_sick = drop_columns(df_sick, ['TBG_measured', 'TBG'])
-        df_sick = min_max_scaler(df_sick, ['age'])
-        df_sick = one_hot_encoding(df_sick)
-        df_sick = binary_encoding(df_sick)
-        df_sick = fill_nans(df_sick, columns_with_nans)
-        df_sick = min_max_scaler(df_sick, columns_with_nans)
+    df_sick = drop_columns(df_sick, ['TBG_measured', 'TBG'])
+    df_sick = min_max_scaler(df_sick, ['age'])
+    df_sick = one_hot_encoding(df_sick)
+    df_sick = binary_encoding(df_sick)
+    df_sick = fill_nans(df_sick, columns_with_nans)
+    df_sick = min_max_scaler(df_sick, columns_with_nans)
 
-        df_sick = df_sick[[col for col in df_sick if col != 'sick'] + ['sick']]
+    df_sick = df_sick[[col for col in df_sick if col != 'sick'] + ['sick']]
 
-        df_sick.to_csv(f'datasets_processed/sick.csv', index=False)
+    df_sick.to_csv(f'datasets_processed/sick.csv', index=False)
+
+    return df_sick.iloc[:,:-1], df_sick.iloc[:,-1]
 
 def preprocess_grid():
     """
@@ -59,23 +60,28 @@ def preprocess_grid():
 
     return df_grid.iloc[:,:-1], df_grid.iloc[:,-1]
 
-def preprocess_vehicle():
+def preprocess_vowel():
     """
-    Applies the specified preprocessings for the dataset vehicle and stores it in the file datasets_processed/vehicle.csv.
+    Applies the specified preprocessings for the dataset vowel and stores it in the file datasets_processed/vowel.csv.
     :return: dataframe
     """
-    df_vehicle, meta_train = loadarff(f'datasets/vehicle.arff')
+    # Load arff file
+    df_vowel, meta_train = loadarff(f'datasets/vowel.arff')
 
-    df_vehicle = pd.DataFrame(df_vehicle)
+    # Define datasets
+    df_vowel = pd.DataFrame(df_vowel)
 
-    # Move the class to the end to not encode it.
-    df_vehicle = df_vehicle[[col for col in df_vehicle if col != 'Class'] + ['Class']]
-    num_cols = df_vehicle.select_dtypes(include=['number']).columns.tolist()
+    y = df_vowel[['Class']]
+    df_vowel = df_vowel.drop(columns=['Class','Train_or_Test'])
 
-    df_vehicle = min_max_scaler(df_vehicle, num_cols) # Only non-numerical column is the last one.
+    df_vowel = one_hot_encoding(df_vowel)
+    df_vowel = binary_encoding(df_vowel)
+    df_vowel = min_max_scaler(df_vowel)
 
-    df_vehicle.to_csv(f'datasets_processed/vehicle.csv', index=False)
+    df_vowel = pd.concat([df_vowel, y])
 
-    return df_vehicle.iloc[:,:-1], df_vehicle.iloc[:,-1]
+    df_vowel.to_csv(f'datasets_processed/vowel.csv', index=False)
+
+    return df_vowel.iloc[:,:-1], df_vowel.iloc[:,-1]
 
 
